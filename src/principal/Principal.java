78973a85d36f32d95fee662915f982e0e3bbdc8d
Ronny <ronnychamba96@gmail.com>
2021-05-26 00:44:21 -0500

    Clase-main-obejtos

---
+++ src/principal/Principal.java
@@ -0,0 +1,22 @@
+package principal;
+
+import entities.Persona;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Persona ronny = new Persona("Ronny", "Chamba",25, false);
+		Persona rene = new Persona("Rene", "Chamba",23, false);
+		Persona santos = new Persona("Santos", "Chamba",55, true);
+		Persona maria = new Persona("Maria", "Chamba",19, false);
+		Persona mayra = new Persona("Mayra", "Chamba",33, true);
+		Persona jandry = new Persona("Jnadry", "Chamba",13, false);
+		Persona javier = new Persona("Javier", "Chamba",30, true);
+		Persona sebas = new Persona("Sebas", "Chamba",8, false);
+		
+
+	}
+
+}
